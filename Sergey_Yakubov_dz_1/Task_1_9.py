26f47e331206da5a8943cb491b9ca76b057a585f
Sergey Yakubov <iamyakubov@gmail.com>
2021-10-19 04:22:28 +0300

    добавил блок схемы, исправил баг в task_1_9.py

--- Sergey_Yakubov_dz_1/Task_1_9.py
+++ Sergey_Yakubov_dz_1/Task_1_9.py
@@ -7,30 +7,17 @@ try:
 
 	mid = c
 
-	if a != b != c:
-		if c > a > b or b > a > c :
+	if a != b and b!= c and a != c:
+		if (a > b and a < c) or (a > c and a < b):
 			mid = a
-		elif c > b > a or a > b > c :
+		elif (b > a and b < c) or (b > c and b < a):
 			mid = b		
 		print(f'Среднее число: {mid}')
 		# else:  - можно и не добавлять, чтобы не дублировать код
 		# 	mid = c	
 
 	else: 
-		print('Все числа равны друг другу. Введите три разных числа')	
+		print('Введите три разных числа')	
 
 except ValueError:
 	print('Можно вводить только числа.')
-
-
-# # или если не по питонячи
-
-# if a != b and b!= c and a != c:
-# 	if (a > b and a < c) or (a > c and a < b):
-# 		mid = a
-# 	elif (b > a and b < c) or (b > c and b < a): 
-# 		mid = b
-# 	print(f'Среднее число: {mid}')			
-# else: 
-# 	print('Какое-то число или все числа равны друг другу. Введите три разных числа.')
-
